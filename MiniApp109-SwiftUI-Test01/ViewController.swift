e53995f241a432793dd866eb45524ffce3a63794
Kota <maedakoutaa@gmail.com>
2022-11-22 13:45:56 +0900

    Initial Commit

---
+++ MiniApp109-SwiftUI-Test01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniApp109-SwiftUI-Test01
+//
+//  Created by 前田航汰 on 2022/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
